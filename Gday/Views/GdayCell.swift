8fad35fc6d2bde86f77e44416bb3005b0dccd8ae
David Cespedes <ingdavidcespedes@gmail.com>
2016-11-21 19:26:44 -0500

    Add GdayCell

---
+++ Gday/Views/GdayCell.swift
@@ -0,0 +1,17 @@
+//
+//  GdayCell.swift
+//  Gday
+//
+//  Created by David Andres Cespedes on 11/21/16.
+//  Copyright © 2016 David Andres Cespedes. All rights reserved.
+//
+
+import UIKit
+
+class GdayCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+    
+  @IBOutlet weak var gdayTypeLabel: UILabel!
+  @IBOutlet weak var userNameLabel: UILabel!
+  @IBOutlet weak var userRoleLabel: UILabel!
+}
