6eb551ddbb77de4c0638da4c39729e9254d08393
David Cespedes <ingdavidcespedes@gmail.com>
2016-11-24 17:11:10 -0500

    Add globate macros

---
+++ Gday/Utils/Macro.swift
@@ -0,0 +1,19 @@
+//
+//  Macro.swift
+//  Gday
+//
+//  Created by David Andres Cespedes on 11/24/16.
+//  Copyright © 2016 David Andres Cespedes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let screenBounds = UIScreen.main.bounds
+let screenSize   = screenBounds.size
+let screenWidth  = screenSize.width
+let screenHeight = screenSize.height
+let gridWidth : CGFloat = (screenSize.width/2)-5.0
+let navigationHeight : CGFloat = 44.0
+let statubarHeight : CGFloat = 20.0
+let navigationHeaderAndStatusbarHeight : CGFloat = navigationHeight + statubarHeight
